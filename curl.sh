cd0b5c386a393e38fe80bab73e73f6784f56e3e6
Gustavo Quijote <gm9510@gmail.com>
2022-06-21 16:20:43 -0500

    CURl POST update

--- curl.sh
+++ curl.sh
@@ -1,7 +1,8 @@
 curl http://localhost:3000/files | json_pp
 
 #v1: curl -X POST http://localhost:3000/files | json_pp
-curl -X POST http://localhost:3000/files -H 'Content-Type: application/json'  -d '{"name":"my_login","ext":"my_password", "size":4}' | json_pp
+#v2: curl -X POST http://localhost:3000/files -H 'Content-Type: application/json'  -d '{"name":"my_login","ext":"my_password", "size":4}' | json_pp
+curl -X POST http://localhost:3000/files -F "file=@download.pdf" -H 'Content-Type: multipart/form-data'
 
 curl -X GET http://localhost:3000/files/1 | json_pp
 
